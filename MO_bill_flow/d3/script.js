aca5dbd3246ecfc14e45b5f2faeef038321430d0
James Gordon <gordon.je@gmail.com>
2014-05-12 22:58:26 -0500

    Committing basic chart pages plus static, template and d3 dirs.

--- MO_bill_flow/d3/script.js
+++ MO_bill_flow/d3/script.js
@@ -24,18 +24,19 @@ var makeChart = function (chamber, chart) {
     var bar = chart.selectAll("g")
         .data(data)
       .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")" });
 
     bar.append("rect")
-        .attr("width", function(d) { return x(d.all_bills); })
+        .attr("width", function(d) { return d.all_bills} )
         .attr("height", barHeight - 1);
 
-    bar.append("text")
-        .attr("x", function(d) { return x(d.all_bills) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.all_bills; });
-    console.log(chart);
+  //   bar.append("text")
+  //       .attr("x", function(d) { return x(d.all_bills) - 3; })
+  //       .attr("y", barHeight / 2)
+  //       .attr("dy", ".35em")
+  //       .text(function(d) { return d.all_bills; });
+  //   console.log(chart);
+
   });
 
   function type(d) {
